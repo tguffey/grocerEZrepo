2c764a0a1b5bc4ca4a7fbf5b6cd92d38dc33045b
tguffey <36212668+tguffey@users.noreply.github.com>
2023-12-04 15:09:13 -0800

    MySQL Connection
    "Connected MySQL"

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require("express")
 const cors = require("cors")
+const db = require('./database');
 
 const app = express()
 
@@ -8,7 +9,7 @@ app.use(express.json())
 
 const port = 5000
 
-
+// Test GET function
 app.get("/api/hello", async(req,res) => {
     try {
         const message = "hello world"
@@ -19,6 +20,7 @@ app.get("/api/hello", async(req,res) => {
     }
 })
 
+// Test POST function
 app.post("/api/hello_post", async(req,res) =>{
     try{
         console.log(req.body)
@@ -29,8 +31,69 @@ app.post("/api/hello_post", async(req,res) =>{
     }
 })
 
+// Test MySQL connection
+// Select all from users
+app.get("/api/sql", async (req, res) => {
+    try {
+        const [rows, fields] = await db.promise().query("SELECT * FROM users");
+        console.log(rows);
+        // Send the fetched data as the response
+        res.status(200).send(rows);
+    } catch (err) {
+        console.error(err.message, "/api/sql");
+        res.status(500).send({ error: 'Internal Server Error' });
+    }
+});
 
+// Add data to user table
+app.post('/api/sql', async (req, res) => {
+    const { email, bio, country } = req.body;
+    
+    if (email && bio && country) {
+        try {
+            // Use parameterized query to avoid SQL injection
+            const result = await db.promise().query(
+                'INSERT INTO users (email, bio, country) VALUES (?, ?, ?)',
+                [email, bio, country]
+            );
 
+            // Check the result if needed
+            console.log(result);
+
+            res.status(201).send({ msg: 'Created User' });
+        } catch (err) {
+            console.error(err.message, "/api/sql");
+            res.status(500).send({ error: 'Internal Server Error' });
+        }
+    } else {
+        res.status(400).send({ error: 'Bad Request: email, bio, and country are required.' });
+    }
+});
+
+// Delete user from users table
+app.delete("/api/sql/:userId", async (req, res) => {
+    const userId = req.params.userId;
+
+    if (!userId) {
+        return res.status(400).send({ error: 'Bad Request: User ID is required.' });
+    }
+
+    try {
+        const result = await db.promise().query("DELETE FROM users WHERE id = ?", [userId]);
+
+        // Check the result if needed
+        console.log(result);
+
+        if (result[0].affectedRows > 0) {
+            res.status(200).send({ msg: `User with ID ${userId} deleted successfully.` });
+        } else {
+            res.status(404).send({ error: `User with ID ${userId} not found.` });
+        }
+    } catch (err) {
+        console.error(err.message, "/api/sql");
+        res.status(500).send({ error: 'Internal Server Error' });
+    }
+});
 
 
 app.listen(port,()=>{
--- test.py
+++ test.py
@@ -1,7 +1,7 @@
 import requests
 
-baseUrl = "http://localhost:5000/api/hello"
-baseUrl2 = "http://localhost:5000/api/hello_post"
+baseUrl = "http://13.58.91.188:5000/api/hello"
+baseUrl2 = "http://13.58.91.188:5000/api/hello_post"
 
 response = requests.get(baseUrl)
 response2 = requests.post(baseUrl2)
